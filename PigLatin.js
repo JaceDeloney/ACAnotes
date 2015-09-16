f1cc309f5c3e5625059b82549816ad09614b8f5e
Jace Deloney <jacedeloney@gmail.com>
2015-09-15 21:03:38 -0500

    Initial Commit

--- PigLatin.js
+++ PigLatin.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var prompt = require('prompt');
+prompt.start();
+
+prompt.get(['name'], function(error, result){
+	console.log('Hello, ' + result['name'] + '!');
+
+
+	var word = result['name'];
+	var firstletter = word[0];
+	console.log(firstletter)
+
+
+	console.log(word.replace(firstletter, ''));
+
+	var pigLatin = console.log(word.replace(firstletter, '') + firstletter + 'ay');
+
+	// if(breakLetter !== 'a' || 'e' || 'i' || 'o' || 'u') {
+		// continue to next letter in string
+	}
+});
